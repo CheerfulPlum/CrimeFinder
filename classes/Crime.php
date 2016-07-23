6327d082308d60977cb2ba97d7fda6cb62f91cf5
root <cheerfulplum@gmail.com>
2016-07-23 17:09:15 +0100

    Updated comments

--- classes/Crime.php
+++ classes/Crime.php
@@ -1,13 +1,61 @@
 <?php
+/**
+ * Crime class 
+ * Very basic class, just used to represent a crime
+ * Having a class makes in easy to add to in the future
+ *
+ *
+ * PHP version 5
+ *
+ * @author     Luke Pace <cheerfulplum@gmail.com>
+ * @copyright  2016 Luke Pace
+ */
+
 class Crime {
-	
+
+	/**
+	* The category of the crime
+	*
+	* @var string
+	* @access public
+	*/
 	public $category;
 
+	/**
+	* The outcome of the crime
+	*
+	* @var string
+	* @access public
+	*/
 	public $outcome;
 
+	/**
+	* The date of the crime, is anonymized
+	* so it's just month and year
+	*
+	* @var string
+	* @access public
+	*/
 	public $date;
 
+	/**
+	* The location of the crime
+	*
+	* @var array
+	* @access public
+	*/
 	public $location = [];
+
+	/**
+	* Very basic constructor
+	*
+	* Just sets some default values
+	* @return void
+	*
+	* @access public
+	*
+	*
+	*/
 
 	public function __construct(){
 		// Just set some basic properties
--- classes/Postcode.php
+++ classes/Postcode.php
@@ -16,20 +16,72 @@
  * @copyright  2016 Luke Pace
  */
 class Postcode {
-
+	/**
+	* Postcode used to do the crime statistics lookup
+	*
+	* @var string
+	* @access public
+	*/
 	public $postcode;
 
+	/**
+	* The latitude found for the given postcode
+	*
+	* @var float
+	* @access public
+	*/
 	public $latitude;
 
+	/**
+	* The longitude found for the given postcode
+	*
+	* @var float
+	* @access public
+	*/
 	public $longitude;
 
+	/**
+	* An array of the points around the postcode which
+	* form a polygon in which to look for crime
+	*
+	* @var array
+	* @access public
+	*/
 	public $pointsArroundPostcode = [];
 
+	/**
+	* The address of the API we are using to get the lat and long of postcode
+	* @var string
+	*/
 	const POSTCODE_API_LOOKUP = 'https://api.postcodes.io/postcodes/';
 
+	/**
+	* The polar circumference of the earth
+	* @var int
+	*/
 	const POLAR_CIRCUMFERENCE = 110540;
 
+	/**
+	* The equatorial circumference of the earth
+	* @var int
+	*/
 	const EQUATORIAL_CIRCUMFERENCE = 111320;
+
+	/**
+	* Method that queries the postcode API
+	*
+	* This method quries the postcode API which returns back the long
+	* and lat of the postcode, this is then used to generate a
+	* co-ordinate polygon which is then used by the CrimeData class
+	* to lookup in the polygon
+	*
+	* @param string $postcode the postcode being looked up
+	* @return void
+	*
+	* @access public
+	*
+	*
+	*/
 
 	public function __construct($postcode){
 		// file_get_contents is a really rubbish function, would usually use cURL but not all version of PHP come with cURL installed by default
@@ -43,7 +95,29 @@ class Postcode {
 		}
 
 	}
-	public function getGeoPointsAroundPoint($latitude, $longitude, $radius){
+
+	/**
+	* Method that generates a an array of lat and long points
+	*
+	* This method takes the lat and long of the postcode being
+	* checked and generates an array of lat and longs around
+	* the point in which to look for crime
+	* 
+	* Method for getting these points was found on StackOverflow here:
+	* http://stackoverflow.com/questions/2187657/calculate-second-point
+	* -knowing-the-starting-point-and-distance
+	*
+	* @param float $latitude the latitude of the central point
+	* @param float $longitude the longitude of the central point
+	* @param int $radius the radius of the search area
+	* @return void
+	*
+	* @access private
+	*
+	*
+	*/
+
+	private function getGeoPointsAroundPoint($latitude, $longitude, $radius){
 		// Use various bearings to make something that looks like a cirle(ish)
 		$bearings = [0, 45, 90, 135, 180, 225, 270];
 		$latsAndLongs = [];
